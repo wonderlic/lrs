f3475bb3d9bc411579aeca651099a51173da5f5c
Andrey Drebot <a.drebot@gmail.com>
2014-10-23 01:34:47 +0300

    Simple API to store JSON objects

---
+++ api/statements.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+var statements = [];
+
+function add(statement) {
+    statements.push(statement);
+}
+
+function get() {
+    return statements;
+}
+
+module.exports.add = add;
+module.exports.get = get;
--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 ﻿var
     koa = require('koa'),
     logger = require('koa-logger'),
-    router = require('koa-router')
+    route = require('koa-route'),
+    parse = require('co-body'),
+
+    statements = require('./api/statements')
 ;
 
 
 var app = koa();
 
 app.use(logger());
-app.use(router(app));
 
 
-app.get('/about', function *(next) {
+app.use(route.get('/about', function *(){
     this.body = { version: '1.0.2'};
-});
+}));
+
+app.use(route.get('/statements', function *(){
+    this.body = statements.get();
+}));
+
+
+app.use(route.put('/statements', function *(next){
+    if ('PUT' != this.method) return yield next;
+
+    statements.add(yield parse(this))
+
+    this.status = 204;    
+}));
+
 
 app.listen(3000);
