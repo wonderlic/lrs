fa27c95229c183fb4e953bc856996a731b55abcb
Andrey Drebot <a.drebot@gmail.com>
2014-10-23 00:46:42 +0300

    A method to return version was added

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿var
+    koa = require('koa'),
+    logger = require('koa-logger'),
+    router = require('koa-router')
+;
+
+
+var app = koa();
+
+app.use(logger());
+app.use(router(app));
+
+
+app.get('/about', function *(next) {
+    this.body = { version: '1.0.2'};
+});
+
+app.listen(3000);
