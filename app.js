72c43cc9cbe3f6980d0fbae626e20f50dbeae7c9
Andrey Drebot <a.drebot@gmail.com>
2014-10-25 16:04:18 +0000

    'Include X-Experience-API-Version' in response header

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var
     route = require('koa-route'),
     parse = require('co-body'),
 
-    statements = require('./api/statements')
-;
+    statements = require('./api/statements'),
+
+
+    VERSION = '1.0.2';
 
 
 var app = koa();
@@ -13,21 +15,39 @@ var app = koa();
 app.use(logger());
 
 
-app.use(route.get('/about', function *(){
-    this.body = { version: '1.0.2'};
+app.use(function*(next) {
+    var header = 'X-Experience-API-Version';
+    
+    if (this.get(header) && this.get(header).substring(0, 3) === VERSION.substring(0, 3)) {
+        yield next;
+    }
+    else {
+        this.status = 400;
+        this.body = 'Invalid \'X-Experience-API-Version\' header was supplied';
+    }
+
+    this.set(header, VERSION);
+});
+
+app.use(route.get('/xAPI/about', function*() {
+    this.body = {
+        version: '1.0.2'
+    };
 }));
 
-app.use(route.get('/statements', function *(){
+app.use(route.get('/xAPI/statements', function*() {
     this.body = statements.get();
 }));
 
 
-app.use(route.put('/statements', function *(next){
-    if ('PUT' != this.method) return yield next;
+app.use(route.put('/xAPI/statements', function*(next) {
+    if ('PUT' != this.method) {
+        return yield next;
+    }
 
     statements.add(yield parse(this))
 
-    this.status = 204;    
+    this.status = 204;
 }));
 
 
