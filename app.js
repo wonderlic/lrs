70292dd0257312bdd09a84645ec61b3fcc865a5e
Andrey Drebot <a.drebot@gmail.com>
2014-11-12 22:53:50 +0000

    Some refactoring to application and DB provider

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var
     koa = require('koa'),
     logger = require('koa-logger'),
@@ -6,11 +8,12 @@ var
     compress = require('koa-compress'),
     parse = require('co-body'),
     db = require("./db/db"),
+
     VERSION = '1.0.2';
 
 var app = koa();
 
-app.use(compress());
+app.use(compress())
 app.use(cors());
 app.use(logger());
 
@@ -19,6 +22,7 @@ app.use(route.get('/xAPI/about', function*() {
     this.body = {
         version: '1.0.2'
     };
+    this.status = 200;
 }));
 
 app.use(function*(next) {
@@ -28,33 +32,35 @@ app.use(function*(next) {
         yield next;
     }
     else {
-        this.status = 400;
         this.body = 'Invalid \'X-Experience-API-Version\' header was supplied';
+        this.status = 400;
     }
 
     this.set(header, VERSION);
 });
 
 app.use(route.get('/xAPI/statements', function*() {
-    var query = this.request.query || {};
+    var
+        criteria = {},
+        query = this.request.query || {};
 
     if (query.statementId) {
         if (Object.keys(query).length !== 1) {
-            this.status = 400;
             this.body = 'You can not supply other params along with statementId';
-            return;
+            this.status = 400;
         }
         else {
-            var statement = yield db.statements.findOne({ id: query.statementId });
+            var statement =
+                yield db.statements.findOne({
+                    id: query.statementId
+                });
             if (statement) {
-                this.status = 200;
                 this.body = statement;
+                this.status = 200;
             }
         }
     }
     else {
-        var criteria = {};
-
         if (query.verb) {
             criteria['verb.id'] = query.verb;
         }
@@ -62,27 +68,24 @@ app.use(route.get('/xAPI/statements', function*() {
         if (query.activity) {
             criteria['object.id'] = query.activity;
         }
-        
+
         if (query.registration) {
             criteria['context.registration'] = query.registration;
         }
-        
-        var statements = yield db.statements.find(criteria);
+
+        var statements =
+            yield db.statements.find(criteria);
+
         if (statements) {
-            this.status = 200;
             this.body = statements;
+            this.status = 200;
         }
     }
 }));
 
-
-app.use(route.put('/xAPI/statements', function*(next) {
-    yield db.statements.insert(yield parse(this));
-    this.status = 204;
-}));
-
-app.use(route.post('/xAPI/statements', function*(next) {
-    yield db.statements.insert(yield parse(this));
+app.use(route.post('/xAPI/statements', function*() {
+    yield db.statements.insert(
+        yield parse(this));
     this.status = 204;
 }));
 
--- db/db.js
+++ db/db.js
@@ -1,9 +1,13 @@
-var monk = require('monk');
-var wrap = require('co-monk');
+'use strict'
 
-var url = 'mongodb://' + process.env.IP + '/lrs'
+var
+    url = 'mongodb://' + process.env.IP + '/lrs',
 
-var db = monk(url);
+    monk = require('monk'),
+    wrap = require('co-monk'),
 
-db.statements = wrap(db.get('statements'));
-module.exports = db;
+    db = monk(url);
+
+module.exports = {
+    statements: wrap(db.get('statements'))
+};
