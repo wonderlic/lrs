d43ff54451542e49e61a1dbe9b914ddacea8da7d
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2017-01-27 16:29:24 +0000

    Allow override of default dbname (lrs) via DBNAME env variable

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ var monk = require('monk');
 var constants = require('../constants');
 
 var
-    url = 'mongodb://' + (process.env.IP || '127.0.0.1') + '/lrs',
+    url = 'mongodb://' + (process.env.IP || '127.0.0.1') + '/' + (process.env.DBNAME || 'lrs'),
     db = monk(url, { connectTimeoutMS: constants.dbConnectionTimeout, socketTimeoutMS: constants.dbSocketTimeout }),
     statements = db.get('statements'),
     results = db.get('results');
